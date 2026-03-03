2516e55c462bcc103458280c09c56fef8e295d78
code-salee <momosylla1998@gmail.com>
2026-03-03 17:21:39 +0000

    Title and favicon changed sucessfully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
         pathMatch: 'full',
         loadComponent: () => import('./pages/dashboard/dashboard.component').then(c => c.DashboardComponent),
           title:
-            'Angular Ecommerce Dashboard | TailAdmin - Angular Admin Dashboard Template', 
+            'Prediction Crime | Dashboard', 
       },
       {
         path:'calendar',
@@ -21,47 +21,47 @@ export const routes: Routes = [
       {
         path:'profile',
         loadComponent: () => import('./pages/profile/profile.component').then(c => c.ProfileComponent),
-        title:'Angular Profile Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Profile'
       },
       {
         path:'form-elements',
         loadComponent: () => import('./pages/forms/form-elements.component').then(c => c.FormComponent),
-        title:'Angular Form Elements Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Form Elements'
       },
       {
         path:'basic-tables',
         loadComponent: () => import('./pages/tables/basic-tables/basic-tables.component').then(c => c.BasicTablesComponent),
-        title:'Angular Basic Tables Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Basic Tables'
       },
       {
         path:'blank',
         loadComponent: () => import('./pages/blank/blank.component').then(c => c.BlankComponent),
-        title:'Angular Blank Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Blank Page'
       },
       {
         path:'line-chart',
         loadComponent: () => import('./pages/charts/line-chart/line-chart.component').then(c => c.LineChartComponent),
-        title:'Angular Line Chart Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Line Chart'
       },
       {
         path:'bar-chart',
         loadComponent: () => import('./pages/charts/bar-chart/bar-chart.component').then(c => c.BarChartComponent),
-        title:'Angular Bar Chart Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Bar Chart'
       },
       {
         path:'avatars',
         loadComponent: () => import('./pages/ui-elements/avatar-element/avatar-element.component').then(c => c.AvatarElementComponent),
-        title:'Angular Avatars Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Avatars'
       },
       {
         path:'badge',
         loadComponent: () => import('./pages/ui-elements/badges/badges.component').then(c => c.BadgesComponent),
-        title:'Angular Badges Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Badges'
       },
       {
         path:'buttons',
         loadComponent: () => import('./pages/ui-elements/buttons/buttons.component').then(c => c.ButtonsComponent),
-        title:'Angular Buttons Dashboard | TailAdmin - Angular Admin Dashboard Template'
+        title:'Prediction Crime | Buttons'
       }
     ]
   },
@@ -69,17 +69,17 @@ export const routes: Routes = [
   {
     path:'signin',
     loadComponent: () => import('./pages/auth-pages/sign-in/sign-in.component').then(c => c.SignInComponent),
-    title:'Angular Sign In Dashboard | TailAdmin - Angular Admin Dashboard Template'
+    title:'Prediction Crime | Sign In'
   },
   {
     path:'signup',
     loadComponent: () => import('./pages/auth-pages/sign-up/sign-up.component').then(c => c.SignUpComponent),
-    title:'Angular Sign Up Dashboard | TailAdmin - Angular Admin Dashboard Template'
+    title:'Prediction Crime | Sign Up'
   },
   // error pages
   {
     path:'**',
     loadComponent: () => import('./pages/other-page/not-found/not-found.component').then(c => c.NotFoundComponent),
-    title:'Angular NotFound Dashboard | TailAdmin - Angular Admin Dashboard Template'
+    title:'Prediction Crime | Not Found'
   },
 ];
